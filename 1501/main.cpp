bf184eb6299be9c7652963830e8bdae272cf357c
HaoLinLiuJerry <42675201+HaoLinLiuJerry@users.noreply.github.com>
2019-06-26 22:06:17 +0800

    Add files via upload

---
+++ 1501/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double time,money;
+    double sum;
+
+    cin>>time>>money;
+    cout<<time*money<<' '<<time*money*9/10;
+    return 0;
+}
---
+++ 1502/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a,b,c,d;
+   cin>>a>>b>>c>>d;
+   double av;
+   av=(a+b+c+d)/4.0;
+
+   cout<<av;
+    return 0;
+}
